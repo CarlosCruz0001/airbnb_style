fd17a8ede2bdcd2963aaaa390ba426f2e827a535
Carlos <118026975+CarlosCruz0001@users.noreply.github.com>
2023-12-10 22:08:58 -0300

    Add files via upload

---
+++ saibamais.ts
@@ -0,0 +1,9 @@
+/*
+Oi prof, só estou deixando aqui os vídeos que usei como base
+vídeo 1: https://www.youtube.com/watch?v=SydnKbGc7W8
+vídeo 2: https://www.youtube.com/watch?v=TI4v4Y8yRjw
+*/
+/*
+ Prof, também tirei os . dos arquivos prettierrrc e eslintrc.json pois, por serem privados, não estavam indo para o
+ repositório, assim o senhor pode analisá-los 
+ */
